46c6af2f3dcebd8c89cedd70061b2bae33a41f9b
garegare <gorogorobird@gmail.com>
2019-08-19 14:27:49 +0900

    add life game source

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainBoardComponent } from './main-board/main-board.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainBoardComponent
   ],
   imports: [
     BrowserModule,
